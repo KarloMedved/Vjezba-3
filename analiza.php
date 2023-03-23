c0c33b79421d08226b88db41836dbf60b2258693
Karlo Medved <karlomedved@yahoo.com>
2023-03-23 19:43:56 +0000

    Završio?

--- analiza.php
+++ analiza.php
@@ -1,49 +1,50 @@
 <?php
 
-function getWords(){
-        $wordsJson = file_get_contents('words.json');
-        $wordsList = json_decode($wordsJson,true);
-        return $wordsList;
+function vowelCounter(string $word) {
+    $vowelCount = 0;
+    $vowel = ['a','e','i','o','u'];
+    foreach ($vowel as $appearance) {
+        $vowelCount += substr_count(strtolower($word), $appearance);
     }
+    return $vowelCount;    
+}
 
-    function createTable(){
-        $words= getWords();
-        $table = '';
-        foreach ($words as $word) {
-            $table .= '
-            <tr>
-                <td>' . $word['word'] . '</td>
-                <td>' . $word['count'] . '</td>
-                <td>' . $word['consCount'] . '</td>
-                <td>' . $word['vowelCount'] . '</td>
-            </tr>';
-        }
-        return $table;
-    }
+$word = $_POST["word"];
 
-    function addWord($newWord) {
-        $wordsList=getWords();
-        $wordsList[] = [
-            "word" => $newWord,
-            "count" => strlen($newWord),
-            "consCount" => getConsonant($newWord),
-            "vowelCount" => getVowel($newWord)
-        ];
-        $wordsJson = json_encode($wordsList);
-        file_put_contents('words.json',$wordsJson);
-    }
+if(!$word) {
+    echo "<h3>Polje za unos ne može biti prazno!</h3>\n";
+    echo "<button onclick='history.go(-1);'>Povratak na unos </button>";
+}
 
-    function getVowel($word) {
-        $vowel=0;
-        $chars= str_split(strtolower($word));
-        foreach($chars as $char){
-            if($char=='a' || $char=='e' || $char=='i' || $char=='o' || $char=='u'){
-                $vowel++;
-            }
-        }
-        return $vowel;
-    }
-     
-    function getConsonant($word) {
-        return strlen($word)-getVowel($word);
-    }
+else if(ctype_alpha($word)) {
+    $numLetter = strlen($word);
+
+    $numVowels = vowelCounter($word);
+
+    $numConsonants = $numLetter - $numVowels;
+
+    $wordsJson = file_get_contents('words.json');
+
+    $wordsArr = json_decode($wordsJson, true);
+
+    $wordsArr[] = [
+        'word' => $word,
+        'numLetter' => $numLetter,
+        'numVowel' => $numVowels,
+        'numConsonant' => $numConsonants
+    ];
+
+    $wordsJson = json_encode($wordsArr);
+
+    file_put_contents('words.json', $wordsJson);
+
+    header("Location: index.php");
+}
+
+else {
+    echo "<h3>Došlo je do greške pri unosu. Provjerite jeste li ispunili sljedeće uvjete:</h3>\n";
+    echo "<ol><li>Unos mora bitit SAMO 1 riječ - bez razmaka.</li>";
+    echo "<li>Unos ne smije sadržavati brojeve!</li>";
+    echo "<li>Unos mora biti isključivo u slovima američke abecede.</li></ol>";
+    echo "<button onclick='history.go(-1);'>Povratak na unos </button>";
+}
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <table>
         <tr>
             <td style="vertical-align:middle">
-                <form action="analiza.php" method="POST">
+                <form action="analiza.php" method="post">
                     <label>Upišite riječ:</label><br>
                     <input type="text" name="word"><br>
                     <input type="submit" value="Pošalji">
@@ -25,6 +25,20 @@
                         <th>Broj suglasnika</th>
                         <th>Broj samoglasnika</th>
                     </tr>
+                    <?php
+
+                $wordsJson = file_get_contents('words.json');
+                $wordsArr = json_decode($wordsJson, true);
+
+                foreach($wordsArr as $word) {
+                    echo "<tr>";
+                        echo "<td>{$word['word']}</td>";
+                        echo "<td>{$word['numLetter']}</td>";
+                        echo "<td>{$word['numConsonant']}</td>";
+                        echo "<td>{$word['numVowel']}</td>";
+                    echo "</tr>";
+                }
+            ?>
                 </table>
             </td>
         </tr>
