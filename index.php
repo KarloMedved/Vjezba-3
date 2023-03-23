faf4ab6b8fd10e79a029c7ca62d2b1699d2ba498
Karlo Medved <karlomedved@yahoo.com>
2023-03-23 18:45:32 +0000

    proba

--- index.php
+++ index.php
@@ -5,8 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parcijalni ispit 2</title>
+    
 </head>
 <body>
+<table>
+        <tr>
+            <td style="vertical-align:middle">
+                <form action="analiza.php" method="POST">
+                    <label>Upišite riječ:</label><br>
+                    <input type="text" name="word"><br>
+                    <input type="submit" value="Pošalji">
+                </form>
+            </td>
+            <td>
+                <table border="2">
+                    <tr>
+                        <th>Riječ</th>
+                        <th>Broj slova</th>
+                        <th>Broj suglasnika</th>
+                        <th>Broj samoglasnika</th>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
     
 </body>
 </html>
